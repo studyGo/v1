d659ada7983db75eb2ae05a61a64581966c7f6f2
kaleesi <85316319@qq.com>
2014-09-09 16:18:53 +0800

    struct1.go

--- 0.1/struct1.go
+++ 0.1/struct1.go
@@ -2,22 +2,27 @@ package main
 
 import "fmt"
 
-type rect struct{
+type rects struct{
     width, height int
+}
+
+type rect struct{
+    rects
+    demo int
 }
 
 func (r *rect) area() int {
 
-    return r.width * r.height
+    return r.rects.width * r.rects.height
 }
 
 func (r *rect) per() int {
 
-    return 2 * (r.width + r.height)
+    return 2 * (r.rects.width + r.rects.height)
 }
-
 func main() {
-    r := rect{width:100, height:200}
+    r := rect{rects{100, 200}, 400}
+    //r := rect(rects{"width":100})
 
     fmt.Println("area :", r.area())
     fmt.Println("per :", r.per())
